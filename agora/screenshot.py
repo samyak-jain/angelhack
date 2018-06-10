29d14901fa770ad761ef137acdb770bb81848a50
Souldiv <amrut.phadke2016@vitstudent.ac.in>
2018-06-09 22:06:41 -0700

    linux transition

--- agora/screenshot.py
+++ agora/screenshot.py
@@ -1,22 +1,27 @@
+import numpy as np
 import asyncio
 from pyppeteer import launch
 import time
-
+from PIL import Image
+import io
 
 async def main():
-    browser = await launch()
+    browser = await launch({'headless': False})
     page = await browser.newPage()
     # await page.goto('http://192.168.14.147:8000/')
-    await page.goto('https://stackoverflow.com/questions/42279675/syncronous-sleep-into-asyncio-coroutine')
+    await page.goto('http://unsting-relay.netlify.com')
+    asyncio.sleep(3)
 
-    for i in range(3):
+    while(True):
         initial = time.time()
-        await asyncio.sleep(5)
-        await page.screenshot({'path': 'example' + str(i) + '.png'})
-        final = time.time() - initial
-        print(final)
+        img = await page.screenshot()
+        # img = await page.screenshot()
+        # img = Image.frombytes('RGB', (640, 480), img)
+        image = Image.open(io.BytesIO(img)).convert('RGB')
+        print(type(image))
+        img = np.array(image)
+        print("array ", img.shape)
 
     await browser.close()
-
 
 asyncio.get_event_loop().run_until_complete(main())
